c82638b2088acb227be68bb1a4af9db0de5dec47
JoeFikis <86575830+JoeFikis@users.noreply.github.com>
2023-10-08 22:16:04 +0000

    negus

--- src/Comps/Signup/signup.jsx
+++ src/Comps/Signup/signup.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './signup.css'
 
 function Signup() {
   return (
-    <div><h1>HEHE</h1></div>
+    <>
+    <div className="signup-container">
+      <div className="signup-wrapper">
+        <div className="signup-box">
+          <div className="signup-header">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="signup-info">
+                <div className="signup-username">
+                 <label htmlFor="name">Username</label>
+                <input type="text" id="name" />
+                </div>
+                <div className="signup-password">
+                <label htmlFor="name">Password</label>
+                <input type="text" id="name" />
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
